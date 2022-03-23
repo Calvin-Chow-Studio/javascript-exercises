889042c3cc1113f2ca436f4c61e087570568606e
Calvin-Chow-Studio <chowjm.calvin@gmail.com>
2022-03-24 01:31:27 +0800

    All passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+    let concatString = '';
+    for (i = 0; i < num; i++) {
+        concatString += string;
+    } if (num < 0) {
+        return "ERROR";
+    } else {
+        return concatString;
+    }
+}
 
-};
+repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
