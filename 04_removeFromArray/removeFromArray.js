c3287877e8c467610e5066a4300973d2c873e7e6
Calvin-Chow-Studio <chowjm.calvin@gmail.com>
2022-03-29 17:58:54 +0800

    Completed 04 removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...num) {
+    const length = array.length
+    if (num.length > 0) {
+    for (i = 0; i < length; i++) {
+        if (array.indexOf(num[i]) === -1) continue;
+        let findNum = array.indexOf(num[i]);
+        array.splice(findNum, 1) }
+    } else {
+        let findNum = array.indexOf(num[0]);
+        array.splice(findNum, 1);
+        }
+        return array
+    }
 
-};
+
+
+//Remember to store the elements of the array in a variable, so the loop and iterate through all
 
 // Do not edit below this line
 module.exports = removeFromArray;
